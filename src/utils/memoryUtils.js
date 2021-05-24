55794a9760bc8914065f117a796e7ab9abe57926
1037603562 <1037603562@qq.com>
2021-05-24 14:49:55 +0800

    src

--- src/utils/memoryUtils.js
+++
@@ -1,7 +0,0 @@
-import storageUtils from './storageUtils.js'
-const user = storageUtils.getUser()
-export default {
-    user,//用来存储用户的登录信息，初始值为localstoreage中读取的user
-    product:{},//需要查看的商品对象
-
-}
